2c057d013420141087b4f6dd1785ec6f6b6e18f1
Octavio Rojas <octavio.rojas@pluto.tv>
2024-10-07 14:22:07 -0600

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Octavio Rojas on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
